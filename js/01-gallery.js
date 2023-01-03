57f7ffc3033d2ad39835d463187c8f761dd08b51
Anatoliy-Usov <sigal.kmua@gmail.com>
2023-01-03 03:28:33 +0200

    end

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const allGallery = document.querySelector('.gallery')
+
+function createGalery () {
+   const markup = galleryItems.map(
+    ({original, preview, description}) =>
+        `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+    />
+  </a>
+</div>`,
+   ).join(''); 
+   allGallery.insertAdjacentHTML('beforeend', markup);
+}
+
+createGalery ()
+
+allGallery.addEventListener('click', runFullImg);
+
+function runFullImg (params) {
+    params.preventDefault();
+     
+basicLightbox.create(`
+		<img width="1280" height="855" src=${event.target.dataset.source}>
+	`).show();
+
+}
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery')
+
+function createGalery () {
+   const markup = galleryItems.map(
+    ({original, preview, description}) =>
+        `<a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" /></a>`).join(''); 
+    
+    gallery.innerHTML = markup;
+}
+createGalery(galleryItems);
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
